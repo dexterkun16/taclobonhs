318618612825331a856276b29dbcac1a46f07a3c
dexterkun16 <61641669+dexterkun16@users.noreply.github.com>
2020-03-01 15:43:39 +0800

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 const burger = document.getElementsByClassName('burger')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
+const nav = document.getElementsByTagName('nav')[0]
+const body = document.getElementsByTagName('body')[0]
     //toggle nav
     burger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-burger-active')
+        navLinks.classList.toggle('nav-burger-active');
+        nav.classList.toggle('nav-burger-active_nav-color');
+        body.classList.toggle('body');
     })
     //animate links
